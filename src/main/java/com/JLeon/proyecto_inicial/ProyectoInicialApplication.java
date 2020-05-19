c665b3afb8f104865e12ce23f4b727eb20e4b574
laions1992 <joserleonm1992@gmail.com.com>
2020-05-19 13:24:45 -0400

    inicia el proyecto

---
+++ src/main/java/com/JLeon/proyecto_inicial/ProyectoInicialApplication.java
@@ -0,0 +1,13 @@
+package com.JLeon.proyecto_inicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInicialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JLeon/proyecto_inicial/ProyectoInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JLeon.proyecto_inicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
